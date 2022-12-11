30ef90f1d50df1372f87e5270b69414962d112c6
Abdulhafeez Yusuf <haythepen@gmail.com>
2022-12-11 23:04:49 +0100

    no env

--- project/src/js/model.js
+++ project/src/js/model.js
@@ -1,7 +1,7 @@
 import "regenerator-runtime/runtime"; //Polyfills async functions
 import { API_URL, RES_PER_PAGE, DEFAULT_MEAL } from "./config";
 import { getJSON, sendJSON } from "./helper";
-import { apiKEY } from "../env";
+// import { apiKEY } from "../env";
 
 const KEY = process.env.KEY || apiKEY;
 
