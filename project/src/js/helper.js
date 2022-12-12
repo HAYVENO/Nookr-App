4637075c355c1e5e623bc3a3acd57f5b179247f2
Abdulhafeez Yusuf <haythepen@gmail.com>
2022-12-12 10:53:20 +0100

    i

--- project/src/js/helper.js
+++ project/src/js/helper.js
@@ -13,10 +13,10 @@ export const getJSON = async function (url) {
 	try {
 		console.log(url);
 		const res = await Promise.race([timeout(TIMEOUT_SEC), fetch(url)]);
-		if (!res.ok) console.log(res);
+		if (!res.ok) return console.log(res);
 		data = await res.json();
 		console.log({ data });
-		if (data.status === "fail") console.log(data);
+		if (data.status === "fail") return console.log(data);
 		return data;
 	} catch (err) {
 		console.error(err);
