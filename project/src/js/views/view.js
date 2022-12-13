372d66b4b1e36f6dce7d51b3bb40e4d1d08c040a
Abdulhafeez Yusuf <haythepen@gmail.com>
2022-12-13 14:17:56 +0100

    spinner revamped :rocket:

--- project/src/js/views/view.js
+++ project/src/js/views/view.js
@@ -1,50 +1,45 @@
-import icons from 'url:../../img/icons.svg';
+import icons from "url:../../img/icons.svg";
 
 export default class View {
-  _data;
-  render(data) {
-    if (!data || (Array.isArray(data) && data.length === 0))
-      return this.renderError();
-    this._data = data;
-    const markup = this._generateMarkup();
-    this._clear();
+	_data;
+	render(data) {
+		if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
+		this._data = data;
+		const markup = this._generateMarkup();
+		this._clear();
 
-    // this._parentElement.insertAdjacentHTML('afterbegin', markup);
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
+		// this._parentElement.insertAdjacentHTML('afterbegin', markup);
+		this._clear();
+		this._parentElement.insertAdjacentHTML("afterbegin", markup);
+	}
 
-  _clear() {
-    this._parentElement.innerHTML = '';
-  }
+	_clear() {
+		this._parentElement.innerHTML = "";
+	}
 
-  renderSpinner() {
-    const html = `<div class="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-        </div> `;
-    return (this._parentElement.innerHTML = html);
-  }
+	renderSpinner() {
+		const html = `<div class="spinner"><div></div></div>`;
+		return (this._parentElement.innerHTML = html);
+	}
 
-  renderError(errMessage = this._errorMessage) {
-    const markup = `  
+	renderError(errMessage = this._errorMessage) {
+		const markup = `  
        <div class="error">
              <div>
                <svg>
                  <use href="${icons}#icon-${
-      this._type === 'bookmarksView' ? 'smile' : 'alert-triangle'
-    }"></use>
+			this._type === "bookmarksView" ? "smile" : "alert-triangle"
+		}"></use>
                </svg>
              </div>
              <p>${errMessage}</p>
        </div> `;
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
+		this._clear();
+		this._parentElement.insertAdjacentHTML("afterbegin", markup);
+	}
 
-  renderMessage(message = this._message) {
-    const markup = `
+	renderMessage(message = this._message) {
+		const markup = `
       <div class="message">
         <div>
           <svg>
@@ -54,7 +49,7 @@ export default class View {
         <p>${message}</p>
       </div>
     `;
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
+		this._clear();
+		this._parentElement.insertAdjacentHTML("afterbegin", markup);
+	}
 }
